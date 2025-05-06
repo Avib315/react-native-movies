2a165a539c13870db4d0a467211d5f837033acf6
user <user@DESKTOP-8867Q6H>
2025-05-06 20:06:34 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light:{
+          100:"#D6C6FF",
+          200:"#A8B5DB",
+          300:"#9CA4AB",
+        },
+        dark:{
+          100:"#221f3d",
+          200:"#0F0D23",
+        },
+        accent: "#AB8BFF",
+        neutral: "#374151",
+        "base-100": "#FFFFFF",
+        info: "#3ABFF8",
+        success: "#36D399",
+        warning: "#FBBD23",
+        error: "#F87272",
+      },
+    },
+  },
+  plugins: [],
+}
+
